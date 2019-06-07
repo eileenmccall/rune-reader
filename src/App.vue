283baa0df40cffe614636d1074bf0e91fa8d1371
Sophie McCall <sophie@fabulas.io>
2019-06-06 22:38:20 -0500

    added fonts and changed background color, light css reset

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+            <div class="glyph fs1">
+            <div class="clearfix pbs">
+                <svg class="icon icon-Algiz"><use xlink:href="#icon-Algiz"></use></svg>
+            </div>
+        </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,12 +22,22 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #262937;
+  height: 100vh;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Skranji', cursive;
 }
 </style>
